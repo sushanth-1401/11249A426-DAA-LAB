08886ee24470bef29cfedb8cfe8c123f179286a7
Dhurjati V B K Sai Sushanth <11249a426@kanchiuniv.ac.in>
2026-04-22 23:09:13 +0530

    Add files via upload

---
+++ 11249A426 DAA LAB PROGRAMS/Ex-4 Fibonacci Series/Ex-4 Fibonacci Series.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+int fib_rec(int n) {
+    if (n <= 1)
+        return n;
+    return fib_rec(n - 1) + fib_rec(n - 2);
+}
+int fib_iter(int n) {
+    int a = 0, b = 1, c, i;
+    if (n == 0) return 0;
+    for (i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Recursive Fibonacci: %d\n", fib_rec(n));
+    printf("Iterative Fibonacci: %d\n", fib_iter(n));
+    
+    printf("\nPress ESC to exit...\n");
+    while (getch() != 27);
+    
+    return 0;
+}
